2cecb8b3f5e56dd97538142b8f02ef87ddaf5e49
Turkay Tunc <turkaytunc00@gmail.com>
2020-09-06 16:49:54 +0300

    Update

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hey');
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Started!</h1>');
+});
+
+app.listen(3000, 'localhost', () => {
+  console.log('server listen port 3000');
+});
